16affe5d78301818e3d5e1be408db419af240032
Anssi Ahola <anssia.ahola@gmail.com>
2021-01-27 13:30:06 +0200

    adds individual test for listing test for saving the listing not yet implemented, results in 404: Resource not found

--- discogs_client/tests/test_models.py
+++ discogs_client/tests/test_models.py
@@ -150,13 +150,6 @@ class ModelsTestCase(DiscogsClientTestCase):
         # Fetch users inventory from the filesystem
         u = self.d.user('example')
         self.assertEqual(len(u.inventory), 1)
-        listing = u.inventory[0]
-        self.assertEqual(listing.status, 'For Sale')
-        self.assertEqual(listing.price.value, 149.99)
-        self.assertEqual(listing.allow_offers, True)
-        self.assertEqual(listing.id, 150899904)
-        self.assertEqual(listing.seller.username, 'example')
-        self.assertEqual(listing.release.id, 2992668)
 
         # Stub out expected responses
         self.m._fetcher.fetcher.responses = {
@@ -181,6 +174,43 @@ class ModelsTestCase(DiscogsClientTestCase):
         self.assertEqual(method, 'POST')
         self.assertEqual(url, '/marketplace/listings')
         self.assertEqual(data['release_id'], '1')
+
+    def test_listing(self):
+        """Listing can be manipulated"""
+        # Fetch users inventory from the filesystem
+        u = self.d.user('example')
+        self.assertEqual(len(u.inventory), 1)
+
+        # Fetch listing
+        listing = u.inventory[0]
+
+        # Test fetching listing information
+        self.assertEqual(listing.status, 'For Sale')
+        self.assertEqual(listing.price.value, 149.99)
+        self.assertEqual(listing.allow_offers, True)
+        self.assertEqual(listing.id, 150899904)
+        self.assertEqual(listing.seller.username, 'example')
+        self.assertEqual(listing.release.id, 2992668)
+
+        # Test manipulating listing
+        listing.status = 'Draft'
+        listing.price = 1.99
+        listing.allow_offers = False
+        listing.release = 1
+        expected = {
+            'status': 'Draft',
+            'price': 1.99,
+            'allow_offers': False,
+            'release_id': 1,
+        }
+        self.assertEqual(listing.changes, expected)
+
+        # Saving results in HTTPError: 404: Resource not found
+        # listing.save()
+        # method, url, data, headers = self.m._fetcher.last_request
+        # self.assertEqual(method, 'POST')
+        # self.assertEqual(url, '/marketplace/listings/150899904')
+        # self.assertEqual(data['listing_id'], '150899904')
 
     def test_collection(self):
         """Collection folders can be manipulated"""
