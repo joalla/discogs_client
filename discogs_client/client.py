67a4dc9a889d69fc58c9299089de6f1f134fd4a6
Anssi Ahola <anssia.ahola@gmail.com>
2021-01-27 06:29:57 +0200

    removes commented out 'Content-Type'

--- discogs_client/client.py
+++ discogs_client/client.py
@@ -96,7 +96,6 @@ class Client:
 
         if data:
             headers['Content-Type'] = 'application/json'
-            # headers['Content-Type'] = 'application/x-www-form-urlencoded'
 
         content, status_code = self._fetcher.fetch(self, method, url, data=data, headers=headers)
 
