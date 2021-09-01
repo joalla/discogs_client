2ba216569e736cb988470f40bdfbae75a015629a
alistair.hughes <ali.fhughes@gmail.com>
2021-09-01 08:45:21 +0100

    wip restructuring and moving to poetry

---
+++ src/discogs_client/__init__.py
@@ -0,0 +1,8 @@
+__version_info__ = 2, 2, 2
+__version__ = '2.2.2'
+
+# from discogs_client.client import Client
+# from discogs_client.models import Artist, Release, Master, Label, User, \
+    # Listing, Track, Price, Video, List, ListItem, Inventory, Wantlist, \
+    # WantlistItem, CollectionItemInstance, CollectionFolder, Order, OrderMessage, OrderMessagesList
+# from discogs_client.utils import Condition, Sort, Status
