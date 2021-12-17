374b0064cdcd9481ca80fda8fc8c2c6a5c5b13fe
J0J0 T <jojo@peek-a-boo.at>
2021-12-17 08:30:47 +0100

    Add guide script, rename, clarify
    - bump.sh: Sanity check bump2version package installed.
    - Rename release.sh to pypi.sh to clarify what it does.
    - Move release.txt content into script release_workflow.sh and elaborate
      workflow. No auto-execution of steps/other scripts, just a checklist.
    - Rename release.txt to release_troubleshooting.txt - it now only keeps
      retag.sh/troubleshooting notes.

--- bump.sh
+++ bump.sh
@@ -9,6 +9,9 @@
 #set -x
 PART=$1
 
+which bump2version
+[[ $? != 0 ]] && echo "bump2version package missing."; exit 1
+
 if [ -z $1 ]; then
     echo "Bumps version, commits and tags."
     echo "Usage: ./bump.s <major|minor|patch> [doit]"
