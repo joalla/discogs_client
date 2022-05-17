16642251767c202bee8b2086416f8edaf5858358
J0J0 T <jojo@peek-a-boo.at>
2022-05-17 10:21:22 +0200

    Client.search: Add tests checking kwargs search
    Add tests and corresponding fixtures to proof that passing keyword arguments
    (**fields) is working.

--- discogs_client/tests/test_models.py
+++ discogs_client/tests/test_models.py
@@ -65,6 +65,18 @@ class ModelsTestCase(DiscogsClientTestCase):
         self.assertTrue(isinstance(results[0], Artist))
         self.assertTrue(isinstance(results[1], Release))
 
+    def test_kwargs_search(self):
+        results = self.d.search(artist='trash80')
+        self.assertEqual(len(results), 13)
+        self.assertTrue(isinstance(results[0], Artist))
+        self.assertTrue(isinstance(results[1], Release))
+
+    def test_kwargs_multiterm_search(self):
+        results = self.d.search(title='icarus', artist='trash80')
+        self.assertEqual(len(results), 13)
+        self.assertTrue(isinstance(results[0], Artist))
+        self.assertTrue(isinstance(results[1], Release))
+
     def test_utf8_search(self):
         uni_string = 'caf\xe9'
         try:
